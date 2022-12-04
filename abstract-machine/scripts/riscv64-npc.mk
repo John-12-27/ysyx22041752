9c35006c2b31cb5d7e0204cd727d959d9c63842c
2022_041752 曹旺 <cw@mail.nwpu.edu.cn>
2022-12-04 17:19:17 +0800

    ref is nemu

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -24,8 +24,8 @@ image: $(IMAGE).elf
 nvboard: image
 	$(MAKE) -C $(NPC_HOME) nvboard IMG_BIN=$(IMAGE).bin
 
-#NEMU_SO = $(NEMU_HOME)/build/riscv64-nemu-interpreter-so
-NEMU_SO = $(NEMU_HOME)/tools/spike-diff/build/riscv64-spike-so
+NEMU_SO = $(NEMU_HOME)/build/riscv64-nemu-interpreter-so
+#NEMU_SO = $(NEMU_HOME)/tools/spike-diff/build/riscv64-spike-so
 
 override SIMFLAGS += -b -l $(shell dirname $(IMAGE).elf)/npc-ilog.txt -m $(shell dirname $(IMAGE).elf)/npc-mlog.txt -f $(shell dirname $(IMAGE).elf)/npc-flog.txt -f $(shell dirname $(IMAGE).elf)/$(NAME)-$(ARCH).elf -d $(NEMU_SO)
 
--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -18,23 +18,58 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
-void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) 
+{
+    uint8_t *m = (uint8_t*)buf;
+    if(direction == DIFFTEST_TO_REF) 
+    {
+        for(size_t i = 0; i < n; i++)
+        {
+            paddr_write(addr+i, 1, *(m+i));
+        }
+    } 
+    else 
+    {
+        assert(0);
+    }
 }
 
-void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+void difftest_regcpy(void *dut, bool direction) 
+{
+    CPU_state *d = (CPU_state*)dut;
+    if(direction == DIFFTEST_TO_REF) 
+    {
+        cpu.pc = d->pc;
+        for(int i = 0; i < 32; i++)
+        {
+            cpu.gpr[i] = d->gpr[i];
+        }
+    } 
+    else 
+    {
+        d->pc = cpu.pc;
+        for(int i = 0; i < 32; i++)
+        {
+            d->gpr[i] = cpu.gpr[i];
+        }
+    }
 }
 
-void difftest_exec(uint64_t n) {
-  assert(0);
+extern void cpu_exec(uint64_t n);
+void difftest_exec(uint64_t n) 
+{
+    cpu_exec(n);
 }
 
-void difftest_raise_intr(word_t NO) {
-  assert(0);
+void difftest_raise_intr(word_t NO) 
+{
+    printf("\n");
+    printf("No implement\n");
+    printf("\n");
 }
 
-void difftest_init(int port) {
-  /* Perform ISA dependent initialization. */
-  init_isa();
+void difftest_init(int port) 
+{
+    /* Perform ISA dependent initialization. */
+    init_isa();
 }
