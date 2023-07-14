0f4c611c2dd795bd9dd03aea2ead870176879cb5
2022_041752 曹旺 <cw@mail.nwpu.edu.cn>
2023-07-14 10:00:57 +0800

    added autotest in rv64_test

--- npc/rv64_test.sh
+++ npc/rv64_test.sh
@@ -1,4 +1,15 @@
+RESULT=.result
+touch $RESULT
 for f in ./rv64uim-p/*.bin
 do
-    make sim OPTIONS="-b -d $NEMU_HOME/build/riscv64-nemu-interpreter-so" IMG_BIN="$f" 
+    basename=$(basename "${f%.bin}")
+    echo -e -n "[\033[1;32m$basename\033[0m]" >> $RESULT
+    if make sim OPTIONS="-b -d $NEMU_HOME/build/riscv64-nemu-interpreter-so" IMG_BIN="$f" 
+    then
+        echo -e "\033[1;32m  PASS!\033[0m" >> $RESULT
+    else
+        echo -e "\033[1;31m  FAIL!\033[0m" >> $RESULT
+    fi
 done
+cat $RESULT
+rm -rf $RESULT
